00b6c677ebb3f446fd3f43f960f941610edee4ae
Thales Aparecido Silva Carneiro <thalesapscarneiro@gmail.com>
2022-03-21 14:32:57 -0300

    Substituindo variáveis por funções

--- Deposito_saque.dart
+++ Deposito_saque.dart
@@ -3,38 +3,45 @@ import 'dart:io';
 main() {
   bool condicao = true;
   while (condicao) {
-    print('DIGITE 1 PARA INICIAR');
+    print('SALDO ATUAL');
+    var saldo = 100;
+    print(saldo);
+    print('DIGITE 1 PARA DEPOSITO OU 2 PARA SAQUE');
+
+    var num1 = stdin.readLineSync();
+    var operacao = int.parse(num1!);
+
+    if (operacao == 1) {
+      print('Deposito');
+    } else if (operacao == 2) {
+      print("Saque");
+    }
+    if (operacao == 1) {
+      print("DIGITE O VALOR DO DEPOSITO");
+      var num2 = stdin.readLineSync();
+      var valor1 = int.parse(num2!);
+      var saldofinal = operacaoDepositof(saldo, valor1);
+      print("Saldo atualizado: " + saldofinal.toString());
+    } else if (operacao == 2) {
+      print('DIGITE O VALOR DO SAQUE');
+      var num3 = stdin.readLineSync();
+      var valor2 = int.parse(num3!);
+      var saldofinal2 = operacaoSaquef(saldo, valor2);
+      print("SALDO ATUALIZADO: " + saldofinal2.toString());
+    }
+
     var text = stdin.readLineSync();
     if (text == "sair") {
       print('PROGRAMA FINALIZADO');
       condicao = false;
-    } else {
-      print('SALDO ATUAL');
-      var saldo = 100;
-      print(saldo);
-      print('DIGITE 1 PARA DEPOSITO OU 2 PARA SAQUE');
-
-      var num1 = stdin.readLineSync();
-      var operacao = int.parse(num1!);
-
-      if (operacao == 1) {
-        print('Deposito');
-      } else if (operacao == 2) {
-        print("Saque");
-      }
-      if (operacao == 1) {
-        print("DIGITE O VALOR DO DEPOSITO");
-        var num2 = stdin.readLineSync();
-        var operacaoDeposito = int.parse(num2!);
-        var saldofinal = saldo + operacaoDeposito;
-        print("Saldo atualizado: " + saldofinal.toString());
-      } else if (operacao == 2) {
-        print('DIGITE O VALOR DO SAQUE');
-        var num3 = stdin.readLineSync();
-        var operacaoSaque = int.parse(num3!);
-        var saldofinal2 = saldo - operacaoSaque;
-        print("SALDO ATUALIZADO: " + saldofinal2.toString());
-      }
     }
   }
+}
+
+int operacaoDepositof(int saldo, int valor1) {
+  return saldo + valor1;
+}
+
+int operacaoSaquef(int saldo, int valor2) {
+  return saldo - valor2;
 }
